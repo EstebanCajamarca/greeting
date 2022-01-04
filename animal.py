837457f3b760eaff7ca8e4f559f6ea637e3a1c47
Esteban Cajamarca <cajamare@oregonstate.edu>
2022-01-03 20:34:33 -0600

    Changed author name and tested code for proper functioning.

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Esteban Cajamarca
+# GitHub username: EstebanCajamarca
+# Date: 1/3/22/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
