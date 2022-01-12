346a7e55ce3f3489351fa4404d6cdbc367edfecc
EstebanCajamarca <cajamare@oregonstate.edu>
2022-01-11 21:26:57 -0600

    Changed author name and tested code for proper functioning.

---
+++ average.py
@@ -0,0 +1,13 @@
+# Author: Esteban Cajamarca
+# GitHub username: EstebanCajamarca
+# Date: 1/11/2022
+# Description: Asks the user to enter five numbers
+# and then prints out the average of those numbers.
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The average of those numbers is.")
+print((num_1+num_2+num_3+num_4+num_5)/5)
